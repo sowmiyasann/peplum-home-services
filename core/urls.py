fc6c72fb4a91a528cf4c6580e25490fdbfefdeb4
sowmiya <sowmiya.3sss@gmail.com>
2026-06-29 17:01:19 +0530

    Create urls.py

---
+++ core/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path
+
+from services.views import home
+
+
+urlpatterns=[
+
+path(
+'admin/',
+admin.site.urls
+),
+
+path(
+'',
+home
+)
+
+]
