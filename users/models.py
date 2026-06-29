2ea87be6e7bc19771aa07074dfb2f09dd5c3e622
sowmiya <sowmiya.3sss@gmail.com>
2026-06-29 18:19:56 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -2,23 +2,39 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class User(
-AbstractUser
-):
+class User(AbstractUser):
 
-    ROLE=[
+    ROLE = [
 
-('customer','Customer'),
+        ('customer', 'Customer'),
 
-('provider','Provider')
+        ('provider', 'Provider')
 
-]
+    ]
 
-    role=models.CharField(
-        max_length=30,
-        choices=ROLE
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE,
+        default='customer'
     )
 
-    phone=models.CharField(
-        max_length=20
+    phone = models.CharField(
+        max_length=20,
+        blank=True
     )
+
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="custom_user_groups",
+        blank=True
+    )
+
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="custom_user_permissions",
+        blank=True
+    )
+
+    def __str__(self):
+
+        return self.username
