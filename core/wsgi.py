7f3ccae7fc84a649a48c0a92a982bbe76e3e5bc6
sowmiya <sowmiya.3sss@gmail.com>
2026-06-29 19:03:21 +0530

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -4,9 +4,9 @@ from django.core.wsgi import get_wsgi_application
 
 
 os.environ.setdefault(
-'DJANGO_SETTINGS_MODULE',
-'core.settings'
+    'DJANGO_SETTINGS_MODULE',
+    'core.settings'
 )
 
-application=
-get_wsgi_application()
+
+application = get_wsgi_application()
