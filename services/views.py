291d142de398507ad0c82dc7857ed41ce9796077
sowmiya <sowmiya.3sss@gmail.com>
2026-06-29 17:36:34 +0530

    Update views.py

--- services/views.py
+++ services/views.py
@@ -1,11 +1,42 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+
+from .models import ServiceProvider
 
 
-def home(
-request
-):
+def home(request):
 
     return render(
         request,
         'index.html'
+    )
+
+
+def provider_register(request):
+
+    if request.method=="POST":
+
+        ServiceProvider.objects.create(
+
+            full_name=request.POST['name'],
+
+            email=request.POST['email'],
+
+            phone=request.POST['phone'],
+
+            service=request.POST['service'],
+
+            experience=request.POST['experience'],
+
+            location=request.POST['location']
+
+        )
+
+        return redirect(
+            '/provider-dashboard/'
+        )
+
+    return render(
+        request,
+        'provider-register.html'
     )
