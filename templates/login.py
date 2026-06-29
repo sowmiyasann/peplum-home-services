bfa02e41f35bad61f304da3863aca47cac721151
sowmiya <sowmiya.3sss@gmail.com>
2026-06-29 17:14:41 +0530

    Create login.py

---
+++ templates/login.py
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<link rel="stylesheet"
+href="/static/css/style.css">
+
+</head>
+
+<body>
+
+<div class="auth">
+
+<h1>
+
+Welcome Back
+
+</h1>
+
+<form>
+
+<input
+placeholder="Email">
+
+<input
+type="password"
+placeholder="Password">
+
+<button>
+
+Login
+
+</button>
+
+</form>
+
+<a href="/register">
+
+Register
+
+</a>
+
+</div>
+
+</body>
+
+</html>
